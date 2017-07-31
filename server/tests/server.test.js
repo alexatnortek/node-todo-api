60a111c63f5244a52ccfe76b3400a8abb1c30680
alexatnortek <alex.belyaev@nortek.com>
2017-07-31 15:22:33 -0700

    add tests for delete route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -99,5 +99,41 @@ describe('GET /todos/:id', () => {
       .expect(404)
       .end(done);
   });
-
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+        Todo.findById(hexId).then((result) => {
+          expect(result).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object id is not valid', (done) => {
+    request(app)
+      .delete(`/todos/123`)
+      .expect(404)
+      .end(done);
+  });
+})
